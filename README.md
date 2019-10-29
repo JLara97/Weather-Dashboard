f22382781467a7a8771a2bddbbc6c5c20cded97e
Jacob Lara <l1a9r9a7@gmail.com>
2019-10-28 19:51:40 -0700

    Changes to html and update to js file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Weather-Dashboard
+
+### Table of Contents
+- [Description](#Description)
+- [Technologies-Libraries](#Technologies-Libraries)
+- [Screenshots](#Screenshots)
+- [Link](#Link)
+​
+### Description
+A daily planner app to help keep track of your day.
+​
+### Technologies-Libraries
+- [Bootstrap](https://getbootstrap.com/) - CSS Framework
+- [Moment.js](https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js) - Date and Time Library
+- [jQuery](https://code.jquery.com/jquery-3.3.1.min.js) - JS Framework
+​
+### Screenshots
+​
+![Image](assets/img/screenshot.PNG)
+​
+​
+### Link
+Check it out! 
+https://jlara97.github.io/Day-Planner-Hw/
--- js/app.js
+++ js/app.js
@@ -15,10 +15,27 @@ $("#search").on("click", function (event) {
         method: "GET"
     }).then(function (data) {
         
+        console.log(data);
+
         pastInput();
 
+        let city = data.name;
         let icon = data.weather[0].icon;
         let temp = data.main.temp;
+        let wind = data.wind.speed;
+        let humid = data.main.humidity;
+        let uvIndex = 0;
+
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + API_KEY + "&lat=" + data.coord.lat + "&lon=" + data.coord.lon,
+            method: "GET"
+        }).then(function (uv) {
+
+            uvIndex = uv.value;
+        })
+
+        $("<div>").addClass("mainWeatherStyle").append($("<h2>").text(city)).append($("<h4>").text("Temperature: " + temp + " F")).append($("<h4>").text("Wind Speed: " + wind + " mph")).append($("<h4>").text("Humidity: " + humid + "%")).append($("<h4>").text("UV Index: " + uvIndex)).appendTo("#mainWeather");
+        
 
     })
 })
